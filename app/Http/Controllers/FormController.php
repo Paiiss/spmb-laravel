9576086ce49a38ab9be4dad22d7938830eca17b1
Paiss <pais.2k05@gmail.com>
2023-12-03 20:03:24 +0700

    Update form controller

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -6,15 +6,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\User;
+use App\Http\Requests\FormUpdateRequest;
+use Illuminate\Support\Facades\Redirect;
 
 class FormController extends Controller
 {
     public function edit(Request $request): Response
     {
+        $user = $user = User::find(auth()->user()->id);
+        if ($user->getForm()->get()->isNotEmpty()) {
+            $form = $user->getForm;
+        } else {
+            $form = $user->getForm()->create();
+        }
         return Inertia::render('Form/Edit', [
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
+            'form' => $form,
         ]);
     }
 
@@ -24,5 +35,17 @@ class FormController extends Controller
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
         ]);
+    }
+
+    public function update(FormUpdateRequest $request): RedirectResponse
+    {
+
+        $user = User::find(auth()->user()->id);
+        // Bagaimana membuat jika form tidak di temukan maka throw error ?
+        if (!$user->getForm()->get()->isNotEmpty()) {
+            $user->getForm()->create();
+        }
+        $user->getForm()->update($request->validated());
+        return Redirect::route('form.edit');
     }
 }
