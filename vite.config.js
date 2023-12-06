c0c60ecd9222338a4675dddf4ece939a0b81561c
Paiss <pais.2k05@gmail.com>
2023-12-06 17:44:08 +0700

    Repair sass

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.js", "resources/scss/app.scss"],
+            input: ["resources/js/app.js", "resources/sass/app.scss"],
             ssr: "resources/js/ssr.js",
             refresh: true,
         }),
