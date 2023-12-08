d649cfb1a3b3ef48682a916b3c56ca2c7a3a609f
Paiss <pais.2k05@gmail.com>
2023-12-08 14:44:56 +0700

    Update

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,13 +5,13 @@ import { Link } from "@inertiajs/vue3";
 
 <template>
     <div
-        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900"
+        class="min-h-screen flex flex-col items-center justify-center pt-6 bg-gray-100 dark:bg-gray-900"
     >
-        <div>
+        <!-- <div>
             <Link href="/">
                 <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
             </Link>
-        </div>
+        </div> -->
 
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
