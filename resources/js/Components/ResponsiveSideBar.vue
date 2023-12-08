0cf6cd62abb822dad6e7b38782e39e1acc39297a
Paiss <pais.2k05@gmail.com>
2023-12-08 14:44:42 +0700

    Create component sidebar

---
+++ resources/js/Components/ResponsiveSideBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from "vue";
+import { Link } from "@inertiajs/vue3";
+
+const props = defineProps({
+    href: {
+        type: String,
+        required: true,
+    },
+    active: {
+        type: Boolean,
+    },
+    icon: {
+        type: String,
+    },
+});
+
+const classes = computed(() =>
+    props.active
+        ? "flex items-center p-2 text-gray-900 bg-gray-200 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition duration-150 ease-in-out"
+        : "flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition duration-150 ease-in-out"
+);
+</script>
+
+<template>
+    <Link :href="href" :class="classes">
+        <i :class="icon"></i> <span class="ms-3"><slot /></span>
+    </Link>
+</template>
