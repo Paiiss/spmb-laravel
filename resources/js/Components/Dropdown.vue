133162b782190f9dafcd2f6fb1408c342adf589f
Paiss <pais.2k05@gmail.com>
2024-01-08 17:57:47 +0700

    update: Dropdown

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,43 +1,43 @@
 <script setup>
-import { computed, onMounted, onUnmounted, ref } from 'vue';
+import { computed, onMounted, onUnmounted, ref } from "vue";
 
 const props = defineProps({
     align: {
         type: String,
-        default: 'right',
+        default: "right",
     },
     width: {
         type: String,
-        default: '48',
+        default: "48",
     },
     contentClasses: {
         type: String,
-        default: 'py-1 bg-white dark:bg-gray-700',
+        default: "py-1 bg-white dark:bg-gray-700",
     },
 });
 
 const closeOnEscape = (e) => {
-    if (open.value && e.key === 'Escape') {
+    if (open.value && e.key === "Escape") {
         open.value = false;
     }
 };
 
-onMounted(() => document.addEventListener('keydown', closeOnEscape));
-onUnmounted(() => document.removeEventListener('keydown', closeOnEscape));
+onMounted(() => document.addEventListener("keydown", closeOnEscape));
+onUnmounted(() => document.removeEventListener("keydown", closeOnEscape));
 
 const widthClass = computed(() => {
     return {
-        48: 'w-48',
+        48: "w-48",
     }[props.width.toString()];
 });
 
 const alignmentClasses = computed(() => {
-    if (props.align === 'left') {
-        return 'ltr:origin-top-left rtl:origin-top-right start-0';
-    } else if (props.align === 'right') {
-        return 'ltr:origin-top-right rtl:origin-top-left end-0';
+    if (props.align === "left") {
+        return "ltr:origin-top-left rtl:origin-top-right start-0";
+    } else if (props.align === "right") {
+        return "ltr:origin-top-right rtl:origin-top-left end-0";
     } else {
-        return 'origin-top';
+        return "origin-top";
     }
 });
 
@@ -51,7 +51,11 @@ const open = ref(false);
         </div>
 
         <!-- Full Screen Dropdown Overlay -->
-        <div v-show="open" class="fixed inset-0 z-40" @click="open = false"></div>
+        <div
+            v-show="open"
+            class="fixed inset-0 z-40"
+            @click="open = false"
+        ></div>
 
         <Transition
             enter-active-class="transition ease-out duration-200"
@@ -68,7 +72,10 @@ const open = ref(false);
                 style="display: none"
                 @click="open = false"
             >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
+                <div
+                    class="rounded-md ring-1 ring-black ring-opacity-5"
+                    :class="contentClasses"
+                >
                     <slot name="content" />
                 </div>
             </div>
