504d72351da3c90d8e83d71f5ba3c9f0136362d9
Paiss <pais.2k05@gmail.com>
2024-01-09 06:58:12 +0700

    create: Wwave controller

--- app/Http/Controllers/WaveController.php
+++ app/Http/Controllers/WaveController.php
@@ -3,8 +3,79 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Wave;
+use Inertia\Response;
+use Inertia\Inertia;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class WaveController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $wave = Wave::all();
+        return Inertia::render('Admin/Wave', [
+            'wave' => $wave
+        ]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'gelombang' => 'required|string|max:255',
+            'tahun_akademik' => 'required|string|max:255',
+            'awal_daftar' => 'required|date',
+            'akhir_daftar' => 'required|date',
+            'tes_tulis' => 'required|date',
+            'tes_kesehatan' => 'required|date',
+            'wawancara' => 'required|date',
+            'active' => 'required|boolean'
+        ]);
+
+        Wave::create([
+            'gelombang' => $request->gelombang,
+            'tahun_akademik' => $request->tahun_akademik,
+            'awal_daftar' => $request->awal_daftar,
+            'akhir_daftar' => $request->akhir_daftar,
+            'tes_tulis' => $request->tes_tulis,
+            'tes_kesehatan' => $request->tes_kesehatan,
+            'wawancara' => $request->wawancara,
+            'active' => $request->active
+        ]);
+
+        return Redirect::back();
+    }
+
+    public function update(Request $request, $id): RedirectResponse
+    {
+        $request->validate([
+            'gelombang' => 'required|string|max:255',
+            'tahun_akademik' => 'required|string|max:255',
+            'awal_daftar' => 'required|date',
+            'akhir_daftar' => 'required|date',
+            'tes_tulis' => 'required|date',
+            'tes_kesehatan' => 'required|date',
+            'wawancara' => 'required|date',
+            'active' => 'required|boolean'
+        ]);
+
+        Wave::where('id', $id)->update([
+            'gelombang' => $request->gelombang,
+            'tahun_akademik' => $request->tahun_akademik,
+            'awal_daftar' => $request->awal_daftar,
+            'akhir_daftar' => $request->akhir_daftar,
+            'tes_tulis' => $request->tes_tulis,
+            'tes_kesehatan' => $request->tes_kesehatan,
+            'wawancara' => $request->wawancara,
+            'active' => $request->active
+        ]);
+
+        return Redirect::back();
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        Wave::where('id', $id)->delete();
+        return Redirect::back();
+    }
 }
