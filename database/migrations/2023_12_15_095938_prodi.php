4028e0f70062c75fb1052d65947223842b2af79a
Paiss <pais.2k05@gmail.com>
2024-01-08 17:57:21 +0700

    create: migration prodi

---
+++ database/migrations/2023_12_15_095938_prodi.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("prodi", function (Blueprint $table) {
+            $table->id();
+            $table->string("nama_prodi")->nullable();
+            $table->string("jenjang")->nullable();
+            $table->string("fakultas")->nullable();
+            $table->string("akreditasi")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
