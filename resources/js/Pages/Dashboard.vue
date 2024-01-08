dca233dd00c5778418a1f201d90c6d58c29fa37b
Paiss <pais.2k05@gmail.com>
2024-01-08 17:58:05 +0700

    update: dashboard page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -38,9 +38,10 @@ const goToForm = () => {
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <h1 class="font-bold text-2xl">
-                            Selamat Datang, {{ $page.props.user.name }}
+                            Selamat Datang di Sistem Pendaftaran Mahasiswa Baru
+                            <br />
+                            {{ $page.props.user.name }} 👋
                         </h1>
-
                         <p v-if="!isAlreadyForm">
                             Apakah anda akan mendaftar sebagai mahasiswa baru?
                             silahkan klik tombol dibawah ini.
@@ -70,7 +71,104 @@ const goToForm = () => {
                     class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        {{ $page.props.user }}
+                        <h1 class="font-bold text-2xl">Panduan pengisian</h1>
+                        <!-- <ul
+                            class="list-decimal list-inside text-gray-500 dark:text-gray-400"
+                        >
+                            <li>
+                                Isi data dengan benar dan sesuai dengan
+                                identitas diri anda di
+                                <Link
+                                    class="font-bold text-blue-500 hover:text-blue-400"
+                                    :href="
+                                        route('form.edit', { id: 'personal' })
+                                    "
+                                    >sini</Link
+                                >.
+                            </li>
+                            <li>
+                                Ajukan pendaftaran untuk gelombang sekarang di
+                                menu
+                                <Link
+                                    class="font-bold text-blue-500 hover:text-blue-400"
+                                    :href="route('form.submission')"
+                                    >Pendaftaran</Link
+                                >.
+                            </li>
+                            <li>
+                                Jika data diri anda sudah lengkap dan benar
+                                silahkan lakukan pembayaran pendaftaran di menu
+                                <Link
+                                    class="font-bold text-blue-500 hover:text-blue-400"
+                                    :href="route('form.payment')"
+                                    >Pembayaran</Link
+                                >, dan upload bukti pembayaran.
+                            </li>
+                        </ul> -->
+                        <ol
+                            class="relative border-s border-gray-200 dark:border-gray-700"
+                        >
+                            <li class="mb-10 ms-4">
+                                <div
+                                    class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                                ></div>
+                                <time
+                                    class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                                    >Langkah pertama</time
+                                >
+                                <h3
+                                    class="text-lg font-semibold text-gray-900 dark:text-white"
+                                >
+                                    Isi data diri
+                                </h3>
+                                <p
+                                    class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"
+                                >
+                                    Lengkapi data diri anda pada menu data diri,
+                                    isi sesuai dengan identitas diri anda.
+                                </p>
+                            </li>
+                            <li class="mb-10 ms-4">
+                                <div
+                                    class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                                ></div>
+                                <time
+                                    class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                                    >Langkah kedua</time
+                                >
+                                <h3
+                                    class="text-lg font-semibold text-gray-900 dark:text-white"
+                                >
+                                    Ajukan pendaftaran gelombang
+                                </h3>
+                                <p
+                                    class="text-base font-normal text-gray-500 dark:text-gray-400"
+                                >
+                                    Pilih gelombang pendaftaran dan pilih jursan
+                                    yang anda inginkan.
+                                </p>
+                            </li>
+                            <li class="ms-4">
+                                <div
+                                    class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                                ></div>
+                                <time
+                                    class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                                    >Langkah ketiga</time
+                                >
+                                <h3
+                                    class="text-lg font-semibold text-gray-900 dark:text-white"
+                                >
+                                    Lakukan pembayaran formulir
+                                </h3>
+                                <p
+                                    class="text-base font-normal text-gray-500 dark:text-gray-400"
+                                >
+                                    Lakukan pembayaran formulir pendaftaran dan
+                                    upload bukti pembayaran.
+                                </p>
+                            </li>
+                        </ol>
                     </div>
                 </div>
             </div>
