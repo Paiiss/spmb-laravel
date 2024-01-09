b77635754fb1e4094c8a78715918480191959032
Paiss <pais.2k05@gmail.com>
2024-01-09 19:01:42 +0700

    create: migration payment

---
+++ database/migrations/2024_01_08_233111_payment.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    public function up(): void
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('bank')->nullable();
+            $table->string('account_name')->nullable();
+            $table->string('account_number')->nullable();
+            $table->string('amount')->nullable();
+            $table->date('date')->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('status')->default(false);
+            $table->string('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment');
+    }
+};
