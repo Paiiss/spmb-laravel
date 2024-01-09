7dea00eab26e9a0af6e67028d29adbd33dce3696
Paiss <pais.2k05@gmail.com>
2024-01-09 19:02:25 +0700

    update: page payment

--- resources/js/Pages/Form/Payment.vue
+++ resources/js/Pages/Form/Payment.vue
@@ -1,55 +1,223 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head, useForm } from "@inertiajs/vue3";
+import { ref } from "vue";
+import { Head, useForm, usePage } from "@inertiajs/vue3";
 import DateInput from "@/Components/DateInput.vue";
 import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import Modal from "@/Components/Modal.vue";
+import FileInput from "@/Components/FileInput.vue";
 
-defineProps();
+defineProps({
+    payment: Object,
+});
+if (usePage().props.payment.length == 0) {
+    usePage().props.payment.push({
+        bank: "-",
+        account_number: "-",
+        amount: "-",
+        created_at: "-",
+        date: "-",
+    });
+}
+
+const dialog = ref(false);
+const dialogType = ref(null); // 0 = add, 1 = edit, 2 = delete
+const dialogItem = ref(null); // id payment for update or delete
+
 const form = useForm({
     image: null,
+    bank: "",
     date: "",
+    account_number: "",
     amount: "",
 });
+
+const open = (i = 0, item = null) => {
+    dialogType.value = i;
+    dialog.value = true;
+    if (i !== 0) dialogItem.value = item;
+    if (i == 1) {
+        form.bank = payment.bank;
+        form.account_number = payment.account_number;
+        form.amount = payment.amount;
+        form.date = payment.date;
+    }
+};
+
+const save = () => {
+    console.log(dialogType.value);
+    console.log(dialogItem.value);
+    if (dialogType.value == 0) {
+        form.post(route("form.payment.store"), {
+            preserveScroll: true,
+            onSuccess: () => close(),
+            onError: (e) => console.log(e),
+            onFinish: () => form.reset(),
+        });
+    } else if (dialogType.value == 1) {
+        form.put(route("form.payment.update", dialogItem.value));
+    } else {
+        form.delete(route("form.payment.destroy", dialogItem.value), {
+            preserveScroll: true,
+            onSuccess: () => close(),
+            onFinish: () => form.reset(),
+        });
+    }
+};
+
+const close = () => {
+    dialog.value = false;
+    dialogType.value = null;
+    dialogItem.value = null;
+    form.image = null;
+    form.reset();
+};
 </script>
 
 <template>
-    <Head title="Pembayaran" />
+    <Head title="Bukti Pembayaran" />
 
     <AuthenticatedLayout>
         <div>
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
-                <div
-                    class="max-w-full p-4 sm:p-8 bg-white dark:bg-gray-800 shadow rounded-lg"
-                >
-                    <h2 class="font-bold text-black text-2xl">
-                        Bukti pembayaran
-                    </h2>
-                    <div class="">
-                        <p class="py-2">
-                            Anda belum mengupload bukti pembayaran.
-                        </p>
+            <div class="max-w-7xl mx-auto">
+                <div class="shadow-md sm:shadow-lg p-4 sm:p-8 bg-white">
+                    <div
+                        class="flex flex-column sm:flex-grow flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4"
+                    >
+                        <header
+                            class="text-lg font-medium text-gray-900 dark:text-gray-100"
+                        >
+                            Bukti pembayaran
+                        </header>
+                        <PrimaryButton @click="open()">Upload</PrimaryButton>
                     </div>
                 </div>
-                <div
-                    class="max-w-full rounded-lg bg-white dark:bg-gray-800 p-4 sm:p-8"
-                >
-                    <div>
-                        <h2 class="font-bold text-black text-2xl">
-                            Formulir pembayaran
+                <div class="relative overflow-x-auto">
+                    <table
+                        class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                    >
+                        <thead
+                            class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                        >
+                            <tr>
+                                <th scope="col" class="px-6 py-3">Bank</th>
+                                <th scope="col" class="px-6 py-3">
+                                    Account Number
+                                </th>
+                                <th scope="col" class="px-6 py-3">Amount</th>
+                                <th scope="col" class="px-6 py-3">Tanngal</th>
+                                <th scope="col" class="px-6 py-3">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                                v-for="item in payment"
+                                :key="item.id"
+                            >
+                                <th
+                                    scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
+                                    {{ item.bank }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ item.account_number }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{
+                                        item.amount == "-"
+                                            ? "-"
+                                            : new Intl.NumberFormat("id-ID", {
+                                                  style: "currency",
+                                                  currency: "IDR",
+                                              }).format(item.amount)
+                                    }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ item.date }}
+                                </td>
+                                <td
+                                    class="px-6 py-4 flex gap-2"
+                                    v-if="item.bank !== '-'"
+                                >
+                                    <button
+                                        @click="open(2, item.id)"
+                                        class="text-red-600 hover:text-red-900"
+                                    >
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </td>
+                                <td class="px-6 py-4 flex gap-2" v-else>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <Modal :show="dialog" @close="close">
+                    <div class="p-6">
+                        <h2
+                            class="text-lg font-medium text-gray-900 dark:text-gray-100"
+                        >
+                            {{
+                                dialogType == 0
+                                    ? "Upload"
+                                    : dialogType == 1
+                                    ? "Edit"
+                                    : "Hapus"
+                            }}
+                            bukti pembayaran
                         </h2>
                         <div class="py-2">
                             <p class="text-gray-500">
-                                Silahkan isi formulir pembayaran dibawah ini.
+                                {{
+                                    dialogType == 0
+                                        ? "Silahkan upload bukti pembayaran dibawah ini."
+                                        : dialogType == 1
+                                        ? "Silahkan edit bukti pembayaran dibawah ini."
+                                        : "Apakah anda yakin ingin menghapus bukti pembayaran ini?"
+                                }}
                             </p>
                         </div>
-                    </div>
-                    <div class="pt-5">
-                        <form action="" class="mt-6 space-y-6">
-                            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                                <div>
+
+                        <div class="mt-6 space-y-6">
+                            <div
+                                class="grid grid-cols-3 md:grid-cols-4 gap-4"
+                                v-if="dialogType !== 2"
+                            >
+                                <div class="col-span-2">
+                                    <InputLabel for="bank" value="Bank" />
+
+                                    <TextInput
+                                        id="bank"
+                                        class="mt-1 block w-full"
+                                        v-model="form.bank"
+                                    />
+                                    <InputError
+                                        class="mt-2"
+                                        :message="form.errors.bank"
+                                    />
+                                </div>
+                                <div class="col-span-2">
+                                    <InputLabel
+                                        for="account_number"
+                                        value="Nomor rekening"
+                                    />
+
+                                    <TextInput
+                                        id="account_number"
+                                        class="mt-1 block w-full"
+                                        v-model="form.account_number"
+                                    />
+                                    <InputError
+                                        class="mt-2"
+                                        :message="form.errors.account_number"
+                                    />
+                                </div>
+                                <div class="col-span-2 md:col-span-2">
                                     <InputLabel
                                         for="date"
                                         value="Tanggal pembayaran"
@@ -57,22 +225,25 @@ const form = useForm({
 
                                     <DateInput
                                         id="date"
-                                        class="mt-1 block w-full"
+                                        ref="date"
                                         v-model="form.date"
+                                        class="mt-1 block w-full"
+                                        placeholder="Tanggal pembayaran"
                                     />
                                     <InputError
                                         class="mt-2"
                                         :message="form.errors.date"
                                     />
                                 </div>
-                                <div>
+                                <div class="col-span-1 md:col-span-2">
                                     <InputLabel
-                                        for="date"
+                                        for="amount"
                                         value="Nominal pembayaran"
+                                        class="overflow-x-hidden"
                                     />
 
                                     <TextInput
-                                        id="date"
+                                        id="amount"
                                         class="mt-1 block w-full [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                                         type="number"
                                         v-model="form.amount"
@@ -82,39 +253,38 @@ const form = useForm({
                                         :message="form.errors.amount"
                                     />
                                 </div>
-                                <div class="col-span-2">
+                                <div class="col-span-4">
                                     <InputLabel
-                                        for="date"
+                                        for="image"
                                         value="Bukti pembayaran"
                                     />
-                                    <input
+                                    <FileInput
+                                        id="image"
                                         class="my-2 block w-full text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                        id="large_size"
-                                        type="file"
+                                        v-model="form.image"
+                                    />
+
+                                    <InputError
+                                        class="mt-2"
+                                        :message="form.errors.image"
                                     />
                                 </div>
                             </div>
                             <div class="flex justify-end gap-4">
-                                <Transition
-                                    enter-active-class="transition ease-in-out"
-                                    enter-from-class="opacity-0"
-                                    leave-active-class="transition ease-in-out"
-                                    leave-to-class="opacity-0"
-                                >
-                                    <p
-                                        v-if="form.recentlySuccessful"
-                                        class="text-sm text-gray-600 dark:text-gray-400 items-center flex gap-2"
-                                    >
-                                        Saved.
-                                    </p>
-                                </Transition>
-                                <PrimaryButton :disabled="form.processing"
-                                    >Save</PrimaryButton
-                                >
+                                <SecondaryButton @click="close" class="ml-2">
+                                    Cancel
+                                </SecondaryButton>
+                                <PrimaryButton @click="save()">{{
+                                    dialogType == 0
+                                        ? "create"
+                                        : dialogType == 1
+                                        ? "update"
+                                        : "delete"
+                                }}</PrimaryButton>
                             </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
+                </Modal>
             </div>
         </div>
     </AuthenticatedLayout>
