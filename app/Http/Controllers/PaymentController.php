719dc7637bec7e170341c2c24ff7d31622330357
Paiss <pais.2k05@gmail.com>
2024-01-09 19:00:03 +0700

    create: model payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,8 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Payment;
+use Inertia\Response;
+use Inertia\Inertia;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Requests\PaymentRequest;
 
 class PaymentController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $payment = Payment::all();
+        return Inertia::render('Admin/Payment', [
+            'payment' => $payment
+        ]);
+    }
+
+    public function store(PaymentRequest $request): RedirectResponse
+    {
+
+
+        $imageName = time() . '.' . $request->image->extension();
+        $request->image->move(public_path('images'), $imageName);
+        $imagePath = 'images/' . $imageName;
+
+        User::find(auth()->user()->id)->payments()->create(
+            [
+                'bank' => $request->bank,
+                'account_number' => $request->account_number,
+                'amount' => $request->amount,
+                'date' => $request->date,
+                'image' => $imagePath,
+            ]
+        );
+        // Payment::create($request->validated());
+
+        return Redirect::back();
+    }
+
+
+    public function userDestroy(string $id): RedirectResponse
+    {
+        // $payment = Payment::find($id);
+        $userPayment = User::find(auth()->user()->id)->payments()->where('id', $id)->first();
+        if ($userPayment) {
+            $userPayment->delete();
+        }
+        return Redirect::back();
+    }
+
+    public function destroy(string $id): RedirectResponse
+    {
+        $payment = Payment::find($id);
+        $payment->delete();
+        return Redirect::back();
+    }
 }
