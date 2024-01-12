32798412fe3a888155c7cdf7b3dc41f3d02188fa
Paiss <pais.2k05@gmail.com>
2024-01-12 20:21:50 +0700

    create: component text area

---
+++ resources/js/Components/TextareaInput.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+defineProps({
+    modelValue: {
+        type: String,
+        required: true,
+    },
+    rows: {
+        type: Number,
+        default: 3,
+    },
+});
+
+defineEmits(["update:modelValue"]);
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute("autofocus")) {
+        input.value.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+    <textarea
+        class="py-3 px-4 block w-full border-gray-300 rounded-lg text-sm focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+        :rows="rows"
+        @input="$emit('update:modelValue', $event.target.value)"
+        ref="input"
+        :value="modelValue"
+    />
+</template>
