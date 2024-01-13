e7007c8dd5c16ee7bbdd4576e0e7089a71fea1fc
Paiss <pais.2k05@gmail.com>
2024-01-13 07:01:44 +0700

    update: fix textarea styling

--- resources/js/Components/TextareaInput.vue
+++ resources/js/Components/TextareaInput.vue
@@ -27,7 +27,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <textarea
-        class="py-3 px-4 block w-full border-gray-300 rounded-lg text-sm focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+        class="py-3 px-4 block w-full border-gray-300 rounded-lg text-sm focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
         :rows="rows"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
