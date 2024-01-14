fb18987efc035321cb41e674efa74720ee049b4c
Paiss <pais.2k05@gmail.com>
2024-01-14 08:48:44 +0700

    create: model exams history

---
+++ app/Models/ExamHistory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExamHistory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'exam_histories';
+
+    protected $fillable = [
+        'exam_id',
+        'user_id',
+        'score',
+        'duration',
+        'is_active',
+        'is_finished',
+        'is_submitted',
+        'started_at',
+        'finished_at',
+    ];
+
+    public function exam()
+    {
+        return $this->belongsTo(Exams::class, 'exam_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
