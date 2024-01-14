40c5ccc4b400a42c7d78ff534f5ffea32329c6e9
Paiss <pais.2k05@gmail.com>
2024-01-14 08:48:35 +0700

    create: model exams question

---
+++ app/Models/ExamQuestion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExamQuestion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'exam_questions';
+
+    protected $fillable = [
+        'exam_id',
+        'question',
+        'option_a',
+        'option_b',
+        'option_c',
+        'option_d',
+        'option_e',
+        'answer',
+    ];
+
+    public function exam()
+    {
+        return $this->belongsTo(Exams::class, 'exam_id', 'id');
+    }
+}
