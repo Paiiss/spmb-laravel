878261d44db19bed2fdd9b00fcd51d2c8e595335
Paiss <pais.2k05@gmail.com>
2024-01-14 08:48:54 +0700

    create: model exams

---
+++ app/Models/Exams.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Exams extends Model
+{
+    use HasFactory;
+
+    protected $table = 'exams';
+
+    protected $fillable = [
+        'name',
+        'duration',
+        'is_active',
+    ];
+
+    public function questions()
+    {
+        return $this->hasMany(ExamQuestion::class, 'exam_id', 'id');
+    }
+
+    // public function histories()
+    // {
+    //     return $this->hasMany(ExamHistory::class, 'exam_id', 'id');
+    // }
+
+    // public function scopeActive($query)
+    // {
+    //     return $query->where('is_active', true);
+    // }
+}
