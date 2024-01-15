d2ba3127163956c17048542c441cf290ea89f645
Paiss <pais.2k05@gmail.com>
2024-01-15 11:53:10 +0700

    create: Exam Question Controller

---
+++ app/Http/Controllers/ExamQuesionController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Requests\ExamQuestionsRequest;
+
+use App\Models\Exams;
+use App\Models\ExamQuestion;
+
+class ExamQuesionController extends Controller
+{
+    public function index(string $exam_id)
+    {
+        $exam = Exams::find($exam_id);
+        if (!$exam) {
+            return Redirect::route('admin.exams');
+        }
+        // dd($exam->questions());
+        return Inertia::render('Admin/Exams/Questions', [
+            'exam' => $exam,
+            'questions' => $exam->questions()->get(),
+        ]);
+    }
+
+    public function store(ExamQuestionsRequest $request, String $exam_id): RedirectResponse
+    {
+        $exam = Exams::find($exam_id);
+        if (!$exam) {
+            return Redirect::route('admin.exams');
+        }
+
+        $exam->questions()->create($request->validated()); //ExamQuestion::create($request->validated());
+
+        return Redirect::back();
+    }
+
+    public function update(ExamQuestionsRequest $request, String $exam_id, String $question_id): RedirectResponse
+    {
+        $question = ExamQuestion::find($question_id);
+        if (!$question) {
+            return Redirect::back();
+        }
+
+        $question->update($request->validated());
+        return Redirect::back();
+    }
+
+    public function destroy(String $exam_id, String $question_id): RedirectResponse
+    {
+        $question = ExamQuestion::find($question_id);
+        if (!$question) {
+            return Redirect::back();
+        }
+
+        $question->delete();
+        return Redirect::back();
+    }
+}
