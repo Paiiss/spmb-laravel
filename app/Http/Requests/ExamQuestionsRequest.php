b03b42704882bcb2196e815b8b5c9f4c6f0db173
Paiss <pais.2k05@gmail.com>
2024-01-15 11:56:13 +0700

    create: question request

---
+++ app/Http/Requests/ExamQuestionsRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ExamQuestionsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    // public function authorize(): bool
+    // {
+    //     if ($this->user()->hasRole('admin')) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'question' => ['required', 'string'],
+            'answer' => ['required', 'string', 'in:a,b,c,d,e'],
+            'option_a' => ['nullable', 'string'],
+            'option_b' => ['nullable', 'string'],
+            'option_c' => ['nullable', 'string'],
+            'option_d' => ['nullable', 'string'],
+            'option_e' => ['nullable', 'string'],
+
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     */
+
+    public function messages(): array
+    {
+        return [
+            'question.required' => 'Pertanyaan harus diisi',
+            'answer.required' => 'Jawaban harus diisi',
+            'answer.in' => 'Jawaban hanya berisi: a, b, c, d, e',
+        ];
+    }
+}
