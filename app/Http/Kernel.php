8b8a014b74ddd9d518b11f53f5802c51a0f3bfc9
Paiss <pais.2k05@gmail.com>
2024-01-16 14:55:08 +0700

    update: register middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\CheckRole::class,
+        'payform' => \App\Http\Middleware\PayForm::class,
     ];
 }
