dab8122c7a9710774aa94b3a0670d4562a07cdd3
Paiss <pais.2k05@gmail.com>
2024-01-19 09:06:07 +0700

    update: set nullable and add attribute

--- app/Http/Requests/FormUpdateRequest.php
+++ app/Http/Requests/FormUpdateRequest.php
@@ -23,15 +23,15 @@ class FormUpdateRequest extends FormRequest
     {
         return [
             // 'name' => ['string', 'max:255'],
-            'gender' => ['string', 'max:255'],
-            'religion' => ['string', 'max:255'],
-            'birth_date' => ['date'],
-            'birth_place_city' => ['string', 'max:255'],
-            'birth_place_province' => ['string', 'max:255'],
-            'birth_place_country' => ['string', 'max:255'],
-            'national_id' => ['integer'],
-            'is_color_blind' => ['boolean'],
-            'is_disability' => ['boolean'],
+            'gender' => ['string', 'max:255', 'nullable'],
+            'religion' => ['string', 'max:255', 'nullable'],
+            'birth_date' => ['date', 'nullable'],
+            'birth_place_city' => ['string', 'max:255', 'nullable'],
+            'birth_place_province' => ['string', 'max:255', 'nullable'],
+            'birth_place_country' => ['string', 'max:255', 'nullable'],
+            'national_id' => ['integer', 'nullable'],
+            'is_color_blind' => ['boolean'], 'nullable',
+            'is_disability' => ['boolean', 'nullable'],
             'disability_note' => ['string', 'max:255', 'nullable'],
             'address' => ['string', 'max:255', 'nullable'],
             'city' => ['string', 'max:255'],
@@ -81,6 +81,82 @@ class FormUpdateRequest extends FormRequest
             // 'option' => ['integer', 'required'],
             // 'option_2' => ['integer', 'nullable'],
 
+
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+
+    public function messages(): array
+    {
+        return [];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'gender' => 'jenis kelamin',
+            'religion' => 'agama',
+            'birth_date' => 'tanggal lahir',
+            'birth_place_city' => 'kota tempat lahir',
+            'birth_place_province' => 'provinsi tempat lahir',
+            'birth_place_country' => 'negara tempat lahir',
+            'national_id' => 'nomor KTP',
+            'is_color_blind' => 'buta warna',
+            'is_disability' => 'disabilitas',
+            'disability_note' => 'catatan disabilitas',
+            'address' => 'alamat',
+            'city' => 'kota',
+            'province' => 'provinsi',
+            'subdistrict' => 'kecamatan',
+            'country' => 'negara',
+            'postal_code' => 'kode pos',
+            'rt' => 'RT',
+            'rw' => 'RW',
+            'phone_number' => 'nomor telepon',
+            'phone_number_alt' => 'nomor telepon alternatif',
+            'last_education' => 'pendidikan terakhir',
+            'education_number' => 'nisn',
+            'education_name' => 'nama sekolah',
+            'education_city' => 'kota sekolah',
+            'education_province' => 'provinsi sekolah',
+            'education_subdistrict' => 'kecamatan sekolah',
+            'education_country' => 'negara sekolah',
+            'education_postal_code' => 'kode pos sekolah',
+            'education_graduation_year' => 'tahun lulus',
+            'education_major' => 'jurusan',
+            'education_grade' => 'nilai rata-rata',
+            'father_name' => 'nama ayah',
+            'father_birth_date' => 'tanggal lahir ayah',
+            'father_place' => 'tempat lahir ayah',
+            'father_last_education' => 'pendidikan terakhir ayah',
+            'father_job' => 'pekerjaan ayah',
+            'father_phone' => 'nomor telepon ayah',
+            'father_email' => 'email ayah',
+            'mother_name' => 'nama ibu',
+            'mother_birth_date' => 'tanggal lahir ibu',
+            'mother_place' => 'tempat lahir ibu',
+            'mother_last_education' => 'pendidikan terakhir ibu',
+            'mother_job' => 'pekerjaan ibu',
+            'mother_email' => 'email ibu',
+            'mother_phone' => 'nomor telepon ibu',
+            'guardian_name' => 'nama wali',
+            'guardian_birth_date' => 'tanggal lahir wali',
+            'guardian_place' => 'tempat lahir wali',
+            'guardian_last_education' => 'pendidikan terakhir wali',
+            'guardian_job' => 'pekerjaan wali',
+            'guardian_email' => 'email wali',
+            'guardian_phone' => 'nomor telepon wali',
+            'affiliate_id' => 'afiliasi',
 
         ];
     }
