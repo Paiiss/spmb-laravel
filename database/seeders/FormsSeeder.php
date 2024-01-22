8499b47d9e9eb1b29fc93fb6aba9c39da7436577
Paiss <pais.2k05@gmail.com>
2024-01-22 16:11:07 +0700

    create: form seeder

---
+++ database/seeders/FormsSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class FormsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userIds = DB::table('users')->pluck('id')->toArray();
+
+
+        $faker = Faker::create('id_ID');
+
+        foreach ($userIds as $userId) {
+            DB::table('forms')->insert([
+                'user_id' => $userId,
+                'no_exam' => $faker->numberBetween(1, 30),
+                'code_registration' => $faker->numberBetween(1, 30),
+                'wave_id' => 1,
+                'option_id' => 1,
+                'is_submitted' => true,
+                'is_paid_registration' => true,
+                'created_at' => $faker->dateTime,
+                'updated_at' => $faker->dateTime,
+            ]);
+        }
+        /* for ($i = 1; $i <= 30; $i++) {
+            DB::table('forms')->insert([
+                
+                'created_at' => $faker->dateTime,
+                'updated_at' => $faker->dateTime,
+            ]);
+        } */
+    }
+}
