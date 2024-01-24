234a30079adad9563158e5ac218eea0c2ffd09a1
Paiss <pais.2k05@gmail.com>
2024-01-24 12:17:10 +0700

    create: middleware only for approved forms

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,5 +68,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\CheckRole::class,
         'payform' => \App\Http\Middleware\PayForm::class,
+        'formapproved' => \App\Http\Middleware\FormApproved::class,
     ];
 }
