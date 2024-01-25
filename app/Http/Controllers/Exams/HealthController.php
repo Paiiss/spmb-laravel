94d35d2ab305fbcc9d92c57cb3cc7d06e66e0313
Paiss <pais.2k05@gmail.com>
2024-01-25 14:12:16 +0700

    update: health controller

--- app/Http/Controllers/Exams/HealthController.php
+++ app/Http/Controllers/Exams/HealthController.php
@@ -4,13 +4,53 @@ namespace App\Http\Controllers\Exams;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
+
+use App\Http\Requests\Exams\HealthRequest;
 
 class HealthController extends Controller
 {
     public function index(Request $reqeust): Response
     {
-        return Inertia::render('Exams/Health/Index');
+        $user = auth()->user()->health;
+        if (!$user) {
+            $user = auth()->user()->health()->create();
+        }
+        // dd($user->getFirstMediaUrl('image') ?? null);
+        return Inertia::render('Exams/Health/Index', [
+            'user' => $user,
+            'image' => $user->getFirstMediaUrl('image') ?? null
+        ]);
+    }
+
+    public function update(HealthRequest $request): RedirectResponse
+    {
+        $user = auth()->user()->health;
+
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+            $user->addMedia($request->file('image'))->toMediaCollection('image');
+        }
+
+        $user->update([
+            'height' => $request->height,
+            'weight' => $request->weight,
+            'blood_type' => $request->blood_type,
+            'blood_pressure' => $request->blood_pressure,
+            'blood_sugar' => $request->blood_sugar,
+            'is_smoking' => $request->is_smoking,
+            'color_blind' => $request->color_blind,
+            'is_disability' => $request->is_disability,
+            'note' => $request->note,
+            'status' => 'submitted',
+        ]);
+
+        session()->flash('alert', [
+            'type' => 'success',
+            'message' => 'Berhasil menyimpan data'
+        ]);
+        return Redirect::back();
     }
 }
