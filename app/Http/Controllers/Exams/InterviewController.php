9b6fc0d2a4d60107c5304262a1fa584bc9fdc236
Paiss <pais.2k05@gmail.com>
2024-01-27 11:31:52 +0700

    update: page interview

--- app/Http/Controllers/Exams/InterviewController.php
+++ app/Http/Controllers/Exams/InterviewController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Exams;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class InterviewController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return Inertia::render('Exams/Interview/Index');
+    }
 }
