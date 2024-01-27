72fac975e894d6a26ca6704ef5846b222e997754
Paiss <pais.2k05@gmail.com>
2024-01-27 11:32:09 +0700

    update: controller exam

--- app/Http/Controllers/Exams/KnowledgeController.php
+++ app/Http/Controllers/Exams/KnowledgeController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Exams;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class KnowledgeController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return Inertia::render('Exams/Knowledge/Index');
+    }
 }
