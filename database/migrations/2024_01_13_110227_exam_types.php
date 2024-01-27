ea0857f009910fd335fdcb68d5455fa22ad4860c
Paiss <pais.2k05@gmail.com>
2024-01-28 00:15:22 +0700

    update: exam migration

--- database/migrations/2024_01_13_110227_exam_types.php
+++ database/migrations/2024_01_13_110227_exam_types.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,6 +15,12 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable()->default(null);
             $table->integer('duration')->default(60);
+            $table->json('allowed')->nullable()->default(null);
+            $table->boolean('shuffle_question')->default(false);
+            $table->boolean('shuffle_answer')->default(false);
+            $table->boolean('show_result')->default(false);
+            $table->string('access_start_time')->nullable()->default(null);
+            $table->string('access_end_time')->nullable()->default(null);
             $table->boolean('is_active')->default(false);
             $table->timestamps();
         });
