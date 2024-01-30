a510ee1ee2627cdc94853217808e36eab7cffd1d
Paiss <pais.2k05@gmail.com>
2024-01-30 16:28:12 +0700

    update: migration exam history

--- database/migrations/2024_01_13_114724_exam_histories.php
+++ database/migrations/2024_01_13_114724_exam_histories.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,6 +19,8 @@ return new class extends Migration
             $table->boolean('is_active')->default(false);
             $table->boolean('is_finished')->default(false);
             $table->boolean('is_submitted')->default(false);
+            $table->string('order_questions')->nullable();
+            $table->json('answers')->nullable();
             $table->timestamp('started_at')->nullable();
             $table->timestamp('finished_at')->nullable();
             $table->timestamps();
