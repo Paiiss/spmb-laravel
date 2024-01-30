c9a20bce143520a5490bb5a36d67f9167827685c
Paiss <pais.2k05@gmail.com>
2024-01-30 16:29:01 +0700

    create: layout cbt

---
+++ resources/js/Layouts/ExamLayout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div class="min-h-screen">
+        <nav class="bg-white sticky top-0 shadow-md">
+            <div
+                class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between"
+            >
+                <div class="flex flex-row">
+                    <div class="flex-shrink-0 flex items-center gap-3">
+                        <Link href="/">
+                            <ApplicationLogo class="block h-9 w-auto" />
+                        </Link>
+                        <h1
+                            class="border-l-2 text-lg inline-flex items-center font-bold text-black px-3 leading-relaxed capitalize"
+                        >
+                            {{ $page.props.web_settings.title_exam }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <main class="p-4 py-10">
+            <slot />
+        </main>
+    </div>
+</template>
