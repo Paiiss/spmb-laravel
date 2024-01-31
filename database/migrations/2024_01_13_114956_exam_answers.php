781b74dcd23176f9b3f0966890d8389e9871e77e
Paiss <pais.2k05@gmail.com>
2024-01-31 13:01:37 +0700

    update: migration add answer

--- database/migrations/2024_01_13_114956_exam_answers.php
+++ database/migrations/2024_01_13_114956_exam_answers.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +15,7 @@ return new class extends Migration
             $table->foreignId('exam_id')->constrained('exams')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('question_id')->constrained('exam_questions')->onDelete('cascade');
-            $table->string('answer');
+            $table->json('answer')->default(json_encode([]));
             $table->timestamps();
         });
     }
