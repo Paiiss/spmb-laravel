b50f726e0719971cc4845b048dd81e0e8de6f193
Paiss <pais.2k05@gmail.com>
2024-01-31 15:43:01 +0700

    update: controller web setting

--- app/Http/Controllers/WebSettingController.php
+++ app/Http/Controllers/WebSettingController.php
@@ -8,12 +8,30 @@ use Illuminate\Support\Facades\Mail;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\WebSettingsRequest;
+use App\Models\WebSettings;
 
 class WebSettingController extends Controller
 {
     public function index(): Response
     {
-        return Inertia::render('Admin/WebSetting');
+        return Inertia::render('Admin/WebSetting', [
+            'webSettings' => WebSettings::first()
+        ]);
+    }
+
+    public function update(WebSettingsRequest $request): RedirectResponse
+    {
+        $webSettings = WebSettings::first();
+        // dd($request->validated());
+        $webSettings->update($request->validated());
+
+        session()->flash('alert', [
+            'type' => 'success',
+            'message' => 'Berhasil menyimpan data'
+        ]);
+
+        return redirect()->back();
     }
 
     public function backup(): RedirectResponse
