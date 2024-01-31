a7a0d5d5b21aee6b9f8b2c76d0901c109b4c337c
Paiss <pais.2k05@gmail.com>
2024-01-31 14:38:14 +0700

    update: deleted user answer migration table

--- config/app.php
+++ config/app.php
@@ -156,16 +156,16 @@ return [
     */
 
     'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
+            /*
+             * Package Service Providers...
+             */
 
-        /*
-         * Application Service Providers...
-         */
+            /*
+             * Application Service Providers...
+             */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+            // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\MediaLibrary\MediaLibraryServiceProvider::class,
