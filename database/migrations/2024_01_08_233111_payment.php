c1805536c76499943dc66b9fec3d14f9356d8e97
Paiss <pais.2k05@gmail.com>
2024-02-01 09:41:04 +0700

    update: removed image field in migration

--- database/migrations/2024_01_08_233111_payment.php
+++ database/migrations/2024_01_08_233111_payment.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,7 +20,6 @@ return new class extends Migration
             $table->string('amount')->nullable();
             $table->date('date')->nullable();
             $table->string('code')->nullable();
-            $table->string('image')->nullable();
             $table->string('type_payment')->default('form');
             $table->string('status')->default('pending'); // pending approved rejected
             $table->string('note')->nullable();
