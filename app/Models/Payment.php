2f34590d86da498d934d3f67aa6a9e5f1acecb5a
Paiss <pais.2k05@gmail.com>
2024-02-01 09:40:12 +0700

    update: payment model uses spatie

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,16 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Payment extends Model
+class Payment extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
     protected $table = 'payment';
 
-    protected $fillable = ['bank', 'account_name', 'account_number', 'amount', 'image', 'date', 'status', 'note', 'type_payment', 'code'];
+    protected $fillable = ['bank', 'account_name', 'account_number', 'amount', 'date', 'status', 'note', 'type_payment', 'code'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('image')->singleFile();
     }
 }
