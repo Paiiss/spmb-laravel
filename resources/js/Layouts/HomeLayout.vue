8ca81db4c5c78f30b6cc9fb46a6543303ccca94c
Paiss <pais.2k05@gmail.com>
2024-02-05 11:16:42 +0700

    update: home layout

--- resources/js/Layouts/HomeLayout.vue
+++ resources/js/Layouts/HomeLayout.vue
@@ -1,7 +1,7 @@
 <script setup>
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 import NavLink from "@/Components/NavLink.vue";
 import NavLinkBtn from "@/Components/NavLinkBtn.vue";
 import Dropdown from "@/Components/Dropdown.vue";
@@ -14,6 +14,27 @@ defineProps({
     },
 });
 
+const scrollY = ref(0);
+
+const handleHeader = () => {
+    // console.log(window.scrollY, window.scrollY > 0);
+    /* const nav = document.querySelector("nav");
+    if (nav) {
+        nav.classList.toggle("sticky", window.scrollY > 0);
+    } */
+    scrollY.value = window.scrollY || 0;
+};
+
+onMounted(() => {
+    console.log("HomeLayout.vue mounted");
+    window.addEventListener("scroll", handleHeader);
+});
+
+onBeforeUnmount(() => {
+    console.log("HomeLayout.vue unmounted");
+    window.removeEventListener("scroll", handleHeader);
+});
+
 const showingNavigationDropdown = ref(false);
 </script>
 
@@ -21,163 +42,127 @@ const showingNavigationDropdown = ref(false);
     <div>
         <div class="flex flex-col min-h-screen">
             <nav
-                class="bg-background-light dark:bg-background-dark sticky top-0 z-40 shadow-md"
+                class="z-40 bg-gray-50 h-20 transition-all delay-150"
+                :class="{
+                    'sticky top-0 bg-opacity-90 scale-100': scrollY > 150,
+                }"
             >
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex flex-row">
-                            <!-- Logo -->
-                            <div class="flex-shrink-0 flex items-center gap-3">
-                                <Link href="/">
-                                    <ApplicationLogo class="block h-9 w-auto" />
-                                </Link>
-                                <h1
-                                    class="border-l-2 text-lg inline-flex items-center font-bold text-white px-3 leading-relaxed"
-                                >
-                                    {{ $page.props.web_settings.title_home }}
-                                </h1>
-                            </div>
-
-                            <!-- Navigation Links -->
-                            <!-- <div
-                                class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
+                <div
+                    class="mx-auto px-4 sm:px-6 lg:px-8 h-full flex justify-between"
+                >
+                    <div class="flex flex-row">
+                        <!-- Logo -->
+                        <div class="flex-shrink-0 flex items-center gap-3">
+                            <Link href="/">
+                                <ApplicationLogo class="block h-9 w-auto" />
+                            </Link>
+                            <h1
+                                class="text-lg md:text-2xl inline-flex items-center font-bold text-gray-900 px-3 leading-relaxed"
                             >
-                                <NavLink
-                                    class="text-white dark:text-white font-bold"
-                                >
-                                    Dashboard
-                                </NavLink>
-                                <NavLink
-                                    class="text-white dark:text-white font-bold"
-                                >
-                                    Repu
-                                </NavLink>
-                                <NavLink
-                                    class="text-white dark:text-white font-bold"
-                                >
-                                    About
-                                </NavLink>
-                            </div> -->
+                                {{ $page.props.web_settings.title_home }}
+                            </h1>
                         </div>
+                    </div>
 
-                        <div class="hidden sm:flex sm:items-center sm:ms-6">
-                            <div class="flex justify-center gap-2">
-                                <Link
-                                    v-if="!$page.props.auth.user"
-                                    :href="route('login')"
-                                    class="inline-flex items-center p-2 text-left border-transparent text-xs uppercase text-white font-bold bg-teal-400 rounded-lg dark:text-gray-800 tracking-widest hover:bg-teal-700 dark:hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out shadow-md"
-                                >
-                                    <i
-                                        class="fa-solid fa-user-graduate pr-2"
-                                    ></i>
-                                    masuk
-                                </Link>
-                                <Link
-                                    v-if="!$page.props.auth.user"
-                                    :href="route('register')"
-                                    class="inline-flex items-center p-2 text-left border-transparent text-xs uppercase text-teal-400 font-bold bg-teal-50 rounded-lg dark:text-gray-800 tracking-widest hover:bg-teal-700 dark:hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out shadow-md"
-                                >
-                                    <i
-                                        class="fa-solid fa-right-to-bracket pr-2"
-                                    ></i
-                                    >daftar</Link
-                                >
-                                <Link
-                                    v-if="$page.props.auth.user"
-                                    :href="route('dashboard')"
-                                    class="inline-flex items-center p-2 text-left border-transparent text-xs uppercase text-white font-bold bg-teal-400 rounded-lg dark:text-gray-800 tracking-widest hover:bg-teal-700 dark:hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out shadow-md"
-                                >
-                                    <i
-                                        class="fa-solid fa-user-graduate pr-2"
-                                    ></i>
-                                    Dashboard</Link
-                                >
-                            </div>
-                        </div>
-
-                        <!-- Hamburger Start -->
-                        <div class="-me-2 flex items-center sm:hidden">
-                            <button
-                                @click="
-                                    showingNavigationDropdown =
-                                        !showingNavigationDropdown
-                                "
-                                class="inline-flex items-center justify-center p-2 rounded-md text-white dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <div class="flex justify-center gap-2">
+                            <Link
+                                v-if="!$page.props.auth.user"
+                                :href="route('login')"
+                                class="inline-flex items-center text-gray-900 hover:text-blue-500 font-bold capitalize"
                             >
-                                <svg
-                                    class="h-6 w-6"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        :class="{
-                                            hidden: showingNavigationDropdown,
-                                            'inline-flex':
-                                                !showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        :class="{
-                                            hidden: !showingNavigationDropdown,
-                                            'inline-flex':
-                                                showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
+                                Masuk
+                            </Link>
+                            <Link
+                                v-if="!$page.props.auth.user"
+                                :href="route('register')"
+                                class="inline-flex items-center p-2 text-left border-transparent text-xs uppercase text-white font-bold bg-blue-500 rounded-lg dark:text-gray-800 tracking-widest hover:bg-blue-700 dark:hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out shadow-md"
+                            >
+                                daftar</Link
+                            >
+                            <Link
+                                v-if="$page.props.auth.user"
+                                :href="route('dashboard')"
+                                class="inline-flex items-center p-2 text-left border-transparent text-xs uppercase text-white font-bold bg-blue-500 rounded-lg dark:text-gray-800 tracking-widest hover:bg-blue-700 dark:hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out shadow-md"
+                            >
+                                Dashboard</Link
+                            >
                         </div>
+                    </div>
+
+                    <!-- Hamburger Start -->
+                    <div class="-me-2 flex items-center sm:hidden">
+                        <button
+                            @click="
+                                showingNavigationDropdown =
+                                    !showingNavigationDropdown
+                            "
+                            class="inline-flex items-center justify-center p-2 rounded-md text-black dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
+                        >
+                            <svg
+                                class="h-6 w-6"
+                                stroke="currentColor"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    :class="{
+                                        hidden: showingNavigationDropdown,
+                                        'inline-flex':
+                                            !showingNavigationDropdown,
+                                    }"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                                <path
+                                    :class="{
+                                        hidden: !showingNavigationDropdown,
+                                        'inline-flex':
+                                            showingNavigationDropdown,
+                                    }"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
                     </div>
                 </div>
 
-                <!-- Responsive Navigation Menu -->
                 <div
                     :class="{
                         block: showingNavigationDropdown,
                         hidden: !showingNavigationDropdown,
                     }"
-                    class="sm:hidden"
+                    class="sm:hidden bg-white"
                 >
                     <div
                         v-if="$page.props.auth.user"
                         class="pt-2 pb-3 space-y-1 px-4"
                     >
-                        <ResponsiveNavLink
-                            :href="route('dashboard')"
-                            class="text-white dark:text-white font-bold rounded-lg"
+                        <ResponsiveNavLink :href="route('dashboard')"
                             >Dashboard
                         </ResponsiveNavLink>
                     </div>
                     <div v-else class="pt-2 pb-3 space-y-1 px-4">
-                        <ResponsiveNavLink
-                            :href="route('login')"
-                            class="text-white dark:text-white font-bold rounded-lg bg-teal-400 dark:bg-teal-500"
+                        <ResponsiveNavLink :href="route('login')"
                             ><i class="fa-solid fa-user-graduate pr-2"></i>
                             Masuk
                         </ResponsiveNavLink>
-                        <ResponsiveNavLink
-                            :href="route('register')"
-                            class="text-white dark:text-white font-bold rounded-lg bg-teal-400 dark:bg-teal-500"
+                        <ResponsiveNavLink :href="route('register')"
                             ><i class="fa-solid fa-right-to-bracket pr-2"></i>
                             Daftar
                         </ResponsiveNavLink>
                     </div>
                 </div>
             </nav>
-            <main class="flex-grow w-full">
+            <main class="flex-grow w-full bg-gray-50">
                 <slot />
             </main>
 
-            <!-- Footer start -->
             <footer class="pt-16 col-end-1 bg-background-light text-white">
                 <div class="container mx-auto">
                     <div
@@ -269,11 +254,6 @@ const showingNavigationDropdown = ref(false);
                     class="text-xs md:text-sm font-bold tracking-wide max-w-7xl mx-auto text-center py-6"
                 >
                     <p>
-                        <!-- &copy; 2023
-                        <a href="https://htp.ac.id/" class="hover:text-teal-500"
-                            >University Hang Tuah Pekanbaru.</a
-                        >
-                        <span>Created by pais hehe</span> -->
                         {{ $page.props.web_settings.footer }}
                     </p>
                 </div>
