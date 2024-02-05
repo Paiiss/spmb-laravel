9aca8ee2b2be0c2b94ea300eb7a9fc9e9039ca6f
Paiss <pais.2k05@gmail.com>
2024-02-05 11:17:15 +0700

    update: remove form delete account

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -44,12 +44,6 @@ defineProps({
                 >
                     <UpdatePasswordForm class="max-w-xl" />
                 </div>
-
-                <div
-                    class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
-                >
-                    <DeleteUserForm class="max-w-xl" />
-                </div>
             </div>
         </div>
     </AuthenticatedLayout>
