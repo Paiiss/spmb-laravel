f279c037c0e91982000b2004c8fcc28bd96d7eca
Paiss <pais.2k05@gmail.com>
2024-02-06 11:24:46 +0700

    update: page interview

--- resources/js/Pages/Exams/Interview/Index.vue
+++ resources/js/Pages/Exams/Interview/Index.vue
@@ -2,20 +2,17 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, Link, usePage, useForm } from "@inertiajs/vue3";
 import { ref, computed } from "vue";
-import TextInput from "@/Components/TextInput.vue";
-import NumberInput from "@/Components/NumberInput.vue";
-import InputError from "@/Components/InputError.vue";
-import InputLabel from "@/Components/InputLabel.vue";
-import Combobox from "@/Components/Combobox.vue";
-import TextareaInput from "@/Components/TextareaInput.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
-import Modal from "@/Components/Modal.vue";
 
 defineProps({
-    exams: {
-        type: Array,
-        default: [] || null,
+    interview: {
+        type: Object,
+        default: {} || null,
+    },
+    auth: {
+        type: Object,
+        default: {} || null,
     },
 });
 </script>
@@ -43,6 +40,100 @@ defineProps({
                                 seseorang terhadap suatu hal.
                             </p>
                         </header>
+                    </div>
+
+                    <div class="flex flex-col gap-4">
+                        <div>
+                            <div class="capitalize">
+                                No Peserta :
+                                <span class="font-bold text-baase">{{
+                                    interview.no_exam
+                                }}</span>
+                            </div>
+
+                            <div class="capitalize">
+                                Nama Peserta :
+                                <span class="font-bold text-baase">{{
+                                    auth.user.name
+                                }}</span>
+                            </div>
+
+                            <div class="capitalize">
+                                Email Peserta :
+                                <span class="font-bold text-baase">{{
+                                    auth.user.email
+                                }}</span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="capitalize">
+                                status wawancara :
+                                <span
+                                    :class="{
+                                        'text-green-500':
+                                            interview.status === 'accepted',
+                                        'text-yellow-500':
+                                            interview.status === 'pending',
+                                        'text-red-500':
+                                            interview.status === 'rejected',
+                                    }"
+                                    class="font-bold text-lg"
+                                    >{{ interview.status }}</span
+                                >
+                            </div>
+
+                            <template v-if="interview.status !== 'pending'">
+                                <div class="capitalize">
+                                    Tanggal Wawancara :
+                                    <span class="font-bold text-baase">{{
+                                        interview.interview_date
+                                    }}</span>
+                                </div>
+
+                                <div class="capitalize">
+                                    Di wawancara oleh :
+                                    <span class="font-bold text-baase">{{
+                                        interview.interviewed_by || ''
+                                    }}</span>
+                                </div>
+                            </template>
+                        </div>
+                        <div>
+                            <header>
+                                <h2
+                                    class="text-lg font-bold text-gray-500   capitalize"
+                                >
+                                    Panduan Wawancara
+                                </h2>
+                                <p class="text-sm text-gray-400">
+                                    Berikut adalah panduan wawancara yang harus
+                                    diikuti oleh peserta.
+                                </p>
+                            </header>
+
+                            <div class="mt-4">
+                                <p class="text-sm text-gray-400">
+                                    <ul
+                                        class="list-disc list-inside text-gray-400"
+                                    >
+                                        <li>
+                                            Peserta wajib mengikuti wawancara
+                                            sesuai dengan jadwal yang telah
+                                            ditentukan.
+                                        </li>
+                                        <li>
+                                            Peserta wajib mengikuti wawancara
+                                            dengan sopan dan santun.
+                                        </li>
+                                        <li>
+                                            Peserta wajib mengikuti wawancara
+                                            dengan penuh kejujuran.
+                                        </li>
+                                    </ul>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
