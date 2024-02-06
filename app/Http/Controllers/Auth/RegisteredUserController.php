17d65e2be0bc1a35a94532c78cd4f901a2f17363
Paiss <pais.2k05@gmail.com>
2024-02-06 14:16:43 +0700

    update: validate register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
-            'phone' => ['required', 'unique'],
+            'phone' => ['required', 'unique:' . User::class, 'regex:/^([0-9\s\-\+\(\)]*)$/', 'min:10'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
