564f1f56ff5a55ee4d673b681cbf5eb5682c9095
Paiss <pais.2k05@gmail.com>
2024-02-06 09:17:49 +0700

    create: middleware exam access

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
+            // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+                // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -69,5 +69,6 @@ class Kernel extends HttpKernel
         'role' => \App\Http\Middleware\CheckRole::class,
         'payform' => \App\Http\Middleware\PayForm::class,
         'formapproved' => \App\Http\Middleware\FormApproved::class,
+        'examaccess' => \App\Http\Middleware\ExamAccess::class,
     ];
 }
---
+++ app/Http/Middleware/ExamAccess.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\RedirectResponse;
+
+class ExamAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $id): Response|RedirectResponse
+    {
+        $prodi = $request->user()->getProdi;
+        if ($prodi[$id] === 1) {
+            return $next($request);
+        }
+
+        return redirect()->route('form.submission');
+    }
+}
