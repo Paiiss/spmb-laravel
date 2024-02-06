8701748443d1ff36130ccbaa4a7a2526281c6a0d
Paiss <pais.2k05@gmail.com>
2024-02-06 11:23:38 +0700

    update: controller interview

--- app/Http/Controllers/Exams/InterviewController.php
+++ app/Http/Controllers/Exams/InterviewController.php
@@ -6,11 +6,29 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+use App\Models\Interviews;
+use App\Models\User;
 
 class InterviewController extends Controller
 {
     public function index(): Response
     {
-        return Inertia::render('Exams/Interview/Index');
+        $user = auth()->user();
+        $interview = $user->getInterviews;
+        if (!$interview) {
+            $interview = Interviews::create([
+                'user_id' => $user->id,
+                'no_exam' => $user->getForm->no_exam,
+                'status' => 'pending'
+            ]);
+        }
+        if ($interview->verificator_id) {
+            $interview->interviewed_by = User::find($interview->verificator_id)->name;
+        }
+        return Inertia::render('Exams/Interview/Index', [
+            'interview' => $interview,
+        ]);
     }
+
 }
