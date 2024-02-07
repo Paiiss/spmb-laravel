8f70de42eda1de86026e6b89ba3f597eba8d2672
Paiss <pais.2k05@gmail.com>
2024-02-08 00:05:02 +0700

    update: make the section disabled

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -6,6 +6,10 @@ defineProps({
         type: String,
         required: true,
     },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 defineEmits(["update:modelValue"]);
@@ -23,7 +27,13 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm hover:bg-gray-50 dark:hover:bg-gray-950"
+        :disabled="disabled"
+        :class="{
+            'cursor-not-allowed': disabled,
+            'bg-gray-100 rounded-md shadow-sm': disabled,
+            'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm hover:bg-gray-50 dark:hover:bg-gray-950':
+                !disabled,
+        }"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
