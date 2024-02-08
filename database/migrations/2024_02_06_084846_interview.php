96d5282e4d55f3dafaf2c00d98d75c8c9d9a56f2
Paiss <pais.2k05@gmail.com>
2024-02-09 01:28:07 +0700

    update: string to json

--- database/migrations/2024_02_06_084846_interview.php
+++ database/migrations/2024_02_06_084846_interview.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('no_exam')->nullable();
             $table->dateTime('interview_date')->nullable();
             $table->text('note')->nullable();
-            $table->foreignId('verificator_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->json('verified_by')->nullable();
             $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
             $table->timestamps();
         });
