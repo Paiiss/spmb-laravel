2c4ca07f07e4beecef108eda2239de6bca1be1e1
Paiss <pais.2k05@gmail.com>
2024-02-08 18:34:31 +0700

    update: migration media spatie

--- database/migrations/2024_01_16_135339_create_media_table.php
+++ database/migrations/2024_01_16_135339_create_media_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('media', function (Blueprint $table) {
@@ -28,5 +27,10 @@ return new class extends Migration
 
             $table->nullableTimestamps();
         });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
     }
 };
