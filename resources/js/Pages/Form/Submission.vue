b6663a03d78ab34ddc764d22d2bbc25a36bcaece
Paiss <pais.2k05@gmail.com>
2024-02-09 02:15:30 +0700

    update: delete status (dev)

--- resources/js/Pages/Form/Submission.vue
+++ resources/js/Pages/Form/Submission.vue
@@ -131,7 +131,6 @@ const progressName = {
 
     <AuthenticatedLayout>
         <div class="flex flex-col gap-3">
-            {{ status }}
             <template v-if="status == 'approved'">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                     <div
