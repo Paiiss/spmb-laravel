c7b1cb982697279755b4343a537b6ec1f8bb1be1
Paiss <pais.2k05@gmail.com>
2024-02-18 02:18:17 +0700

    feat: separate guide

--- resources/js/Pages/Exams/Interview/Index.vue
+++ resources/js/Pages/Exams/Interview/Index.vue
@@ -22,112 +22,122 @@ defineProps({
     <AuthenticatedLayout>
         <div>
             <div class="max-w-7xl mx-auto">
-                <div
-                    class="bg-white dark:bg-gray-800 p-4 sm:p-8 shadow-md rounded-lg sm:shadow-lg"
-                >
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:gap-8">
                     <div
-                        class="flex flex-column sm:flex-row flex-wrap items-center justify-between pb-4"
+                        class="bg-white dark:bg-gray-800 p-4 sm:p-8 shadow-md rounded-lg sm:shadow-lg col-span-2"
                     >
-                        <header>
-                            <h2
-                                class="text-2xl font-bold text-gray-900 dark:text-gray-100 capitalize"
-                            >
-                                Wawancara peserta
-                            </h2>
-                            <p class="text-sm text-gray-400">
-                                Wawancara peserta adalah tes yang bertujuan
-                                untuk mengukur seberapa jauh pengetahuan
-                                seseorang terhadap suatu hal.
-                            </p>
-                        </header>
-                    </div>
-
-                    <div class="flex flex-col gap-4">
-                        <div>
-                            <div class="capitalize">
-                                No Peserta :
-                                <span class="font-bold text-baase">{{
-                                    interview.no_exam
-                                }}</span>
-                            </div>
-
-                            <div class="capitalize">
-                                Nama Peserta :
-                                <span class="font-bold text-baase">{{
-                                    auth.user.name
-                                }}</span>
-                            </div>
-
-                            <div>
-                                Email Peserta :
-                                <span class="font-bold text-base">{{
-                                    auth.user.email
-                                }}</span>
-                            </div>
+                        <div
+                            class="flex flex-column sm:flex-row flex-wrap items-center justify-between pb-4"
+                        >
+                            <header>
+                                <h2
+                                    class="text-2xl font-bold text-gray-900 dark:text-gray-100 capitalize"
+                                >
+                                    Wawancara peserta
+                                </h2>
+                                <p class="text-sm text-gray-400">
+                                    Wawancara peserta adalah tes yang bertujuan
+                                    untuk mengukur seberapa jauh pengetahuan
+                                    seseorang terhadap suatu hal.
+                                </p>
+                            </header>
                         </div>
 
-                        <div>
-                            <div class="capitalize">
-                                status wawancara :
-                                <span
-                                    :class="{
-                                        'text-green-500':
-                                            interview.status === 'approved',
-                                        'text-yellow-500':
-                                            interview.status === 'pending',
-                                        'text-red-500':
-                                            interview.status === 'rejected',
-                                    }"
-                                    class="font-bold text-lg uppercase"
-                                    >{{ interview.status }}</span
-                                >
-                            </div>
-
-                            <template v-if="interview.status !== 'pending'">
+                        <div class="flex flex-col gap-4">
+                            <div>
                                 <div class="capitalize">
-                                    Tanggal Wawancara :
+                                    No Peserta :
                                     <span class="font-bold text-baase">{{
-                                        interview.interview_date
+                                        interview.no_exam
                                     }}</span>
                                 </div>
 
                                 <div class="capitalize">
-                                    Di wawancara oleh :
+                                    Nama Peserta :
+                                    <span class="font-bold text-baase">{{
+                                        auth.user.name
+                                    }}</span>
+                                </div>
+
+                                <div>
+                                    Email Peserta :
+                                    <span class="font-bold text-base">{{
+                                        auth.user.email
+                                    }}</span>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div class="capitalize">
+                                    status wawancara :
                                     <span
-                                        class="font-bold text-base capitalize"
-                                        >{{
-                                            JSON.parse(interview.verified_by)
-                                                .name || ""
-                                        }}</span
+                                        :class="{
+                                            'text-green-500':
+                                                interview.status === 'approved',
+                                            'text-yellow-500':
+                                                interview.status === 'pending',
+                                            'text-red-500':
+                                                interview.status === 'rejected',
+                                        }"
+                                        class="font-bold text-lg uppercase"
+                                        >{{ interview.status }}</span
                                     >
+                                </div>
+
+                                <template v-if="interview.status !== 'pending'">
+                                    <div class="capitalize">
+                                        Tanggal Wawancara :
+                                        <span class="font-bold text-baase">{{
+                                            interview.interview_date
+                                        }}</span>
+                                    </div>
+
+                                    <div class="capitalize">
+                                        Di wawancara oleh :
+                                        <span
+                                            class="font-bold text-base capitalize"
+                                            >{{
+                                                JSON.parse(
+                                                    interview.verified_by
+                                                ).name || ""
+                                            }}</span
+                                        >
+                                    </div>
+                                </template>
+                            </div>
+                            <template
+                                v-if="
+                                    interview.status == 'rejected' &&
+                                    interview.note
+                                "
+                            >
+                                <div
+                                    class="bg-yellow-50 dark:bg-yellow-800 border-l-4 border-yellow-400 p-4 rounded-lg"
+                                >
+                                    <h4
+                                        class="flex items-center text-lg font-semibold text-yellow-600 dark:text-yellow-400"
+                                    >
+                                        <i
+                                            class="fa-solid fa-exclamation-triangle text-yellow-400"
+                                        ></i>
+                                        <span class="ml-2">Perhatian</span>
+                                    </h4>
+
+                                    <p
+                                        class="mt-2 text-sm text-gray-500 dark:text-gray-400"
+                                    >
+                                        {{ interview.note }}
+                                    </p>
                                 </div>
                             </template>
                         </div>
-                        <template
-                            v-if="
-                                interview.status == 'rejected' && interview.note
-                            "
+                    </div>
+                    <div
+                        class="bg-white dark:bg-gray-800 p-4 sm:p-8 shadow-md rounded-lg sm:shadow-lg col-span-1"
+                    >
+                        <div
+                            class="flex flex-column sm:flex-row flex-wrap items-center justify-between pb-4"
                         >
-                            <div
-                                class="bg-yellow-50 dark:bg-yellow-800 border-l-4 border-yellow-400 p-4 rounded-lg"
-                            >
-                                <h4
-                                    class="flex items-center text-lg font-semibold text-yellow-600 dark:text-yellow-400"
-                                >
-                                    <i
-                                        class="fa-solid fa-exclamation-triangle text-yellow-400"
-                                    ></i>
-                                    <span class="ml-2">Perhatian</span>
-                                </h4>
-
-                                <p
-                                    class="mt-2 text-sm text-gray-500 dark:text-gray-400"
-                                >
-                                    {{ interview.note }}
-                                </p>
-                            </div>
-                        </template>
-                        <div v-if="interview.status == 'pending'">
                             <header>
                                 <h2
                                     class="text-lg font-bold text-gray-500 capitalize"
