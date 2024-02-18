a7b30485af2a0b9dd7d27c26f96de3a938cda788
Paiss <pais.2k05@gmail.com>
2024-02-19 00:10:35 +0700

    feat: add appearance and tidy up

--- resources/js/Pages/Submission/Partials/ApprovedForm.vue
+++ resources/js/Pages/Submission/Partials/ApprovedForm.vue
@@ -1,25 +1,75 @@
 <script setup>
+import Card from "@/Components/Card.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import Container from "@/Components/Container.vue";
 defineProps({
-    wave: {
+    form: {
         default: null,
-        type: String,
+        type: Object,
     },
 });
 </script>
 
 <template>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-            <h2 class="font-bold text-left text-black text-2xl capitalize">
-                Pendaftaran
-            </h2>
-
-            <p>
-                Anda sudah mendapatkan akses form pendaftaran tahun ajaran
-                <span class="font-semibold">{{ wave.tahun_akademik }}</span
-                >, silahkan melakukan pendaftaran dengan mengisi form
-                pendaftaran.
-            </p>
-        </div>
-    </div>
+    <Container>
+        <Card
+            title="Pendaftaran"
+            :description="`Data anda sudah di finalisasi pada pendaftaran tahun ajaran ${form.wave.tahun_akademik}, silahkan melanjutkan ke tahap selanjutnya.`"
+        >
+            <div class="grid grid-cols-3 gap-2">
+                <div class="col-span-3 md:col-span-1 flex justify-center">
+                    <img
+                        :src="form.foto"
+                        alt="Foto"
+                        class="w-60 h-80 rounded-lg"
+                    />
+                </div>
+                <div class="col-span-3 md:col-span-2 flex flex-col gap-4">
+                    <div
+                        class="font-bold text-lg text-gray-500 dark:text-gray-300"
+                    >
+                        No Ujian : <span>{{ form.no_exam }}</span>
+                    </div>
+                    <div
+                        class="font-bold text-lg text-gray-500 dark:text-gray-300"
+                    >
+                        <div
+                            class="font-bold text-lg text-gray-500 dark:text-gray-300"
+                        >
+                            Nama : <span>{{ form.name }}</span>
+                        </div>
+                        <div
+                            class="font-bold text-lg text-gray-500 dark:text-gray-300"
+                        >
+                            NISN : <span>{{ form.education_number }}</span>
+                        </div>
+                    </div>
+                    <div
+                        class="font-bold text-lg text-gray-500 dark:text-gray-300"
+                    >
+                        Tempat, Tanggal Lahir :
+                        <span
+                            >{{ form.birth_place_city }},
+                            {{ form.birth_date }}</span
+                        >
+                    </div>
+                    <div
+                        class="font-bold text-lg text-gray-500 dark:text-gray-300"
+                    >
+                        Pilihan Program Studi :
+                        <span>{{ form.prodi }}</span>
+                    </div>
+                    <div
+                        class="font-bold text-lg text-gray-500 dark:text-gray-300"
+                    >
+                        Gelombang :
+                        <span>{{ form.wave.code }}</span>
+                    </div>
+                    <div>
+                        <PrimaryButton> Cetak Kartu Ujian </PrimaryButton>
+                    </div>
+                </div>
+            </div>
+        </Card>
+    </Container>
 </template>
