87800d10a0e0f26568d32f796afdf415c7203dbb
Paiss <pais.2k05@gmail.com>
2024-02-19 00:43:39 +0700

    feat: fixing the function and adding updates for the end of validation

--- app/Http/Controllers/Admin/AdminEndValidation.php
+++ app/Http/Controllers/Admin/AdminEndValidation.php
@@ -12,11 +12,40 @@ class AdminEndValidation extends Controller
 {
     public function index(): Response
     {
+        $data = Form::where('end_status', 'submitted')->orderBy('created_at', 'desc')->with('user', 'prodi', 'wave')->paginate(10)->through(function ($form) {
+            return [
+                'id' => $form->id,
+                'user' => $form->user->name,
+                'email' => $form->user->email,
+                'phone' => $form->user->phone,
+                'status' => $form->status,
+                'end_status' => $form->end_status,
+                'prodi' => $form->prodi->nama_prodi,
+                'wave' => $form->wave->nama_wave,
+            ];
+        });
+
         return Inertia::render(
             'Admin/Verification/End/Index',
             [
-                'forms' => Form::orderBy('created_at', 'desc')->with('user', 'prodi', 'wave')->paginate(10)
+                'forms' => $data
             ]
         );
+    }
+
+    public function update(Request $request, string $id): Response
+    {
+        $request->validate([
+            'status' => ['required', 'in:approved,rejected'],
+        ]);
+
+        $form = Form::find($id);
+        if (!$form) {
+            return redirect()->back();
+        }
+        $form->end_status = $request->status;
+        $form->save();
+
+        return redirect()->back();
     }
 }
