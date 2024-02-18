41e8477c7debed2fef3d3beefbfe54d8ffdc2ddc
Paiss <pais.2k05@gmail.com>
2024-02-19 00:44:39 +0700

    feat: create a component button icon

---
+++ resources/js/Components/IconButton.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+    icon: {
+        type: String,
+        default: "fas fa-plus",
+    },
+    action: {
+        type: Function,
+        default: () => {},
+    },
+    color: {
+        type: String,
+        default: "blue",
+    },
+});
+</script>
+
+<template>
+    <button
+        @click="action"
+        class="flex items-center justify-center w-8 h-8 rounded-full"
+        :class="`text-${color}-500 dark:bg-${color}-500/10 dark:text-${color}-400`"
+    >
+        <i :class="icon"></i>
+    </button>
+</template>
