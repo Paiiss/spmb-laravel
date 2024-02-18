2db72225cb0738888a01a872bc061d25a8f17a19
Paiss <pais.2k05@gmail.com>
2024-02-19 00:01:43 +0700

    feat: create components

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        default: null,
+    },
+    description: {
+        type: String,
+        default: null,
+    },
+});
+</script>
+
+<template>
+    <div
+        class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex flex-col gap-4"
+    >
+        <div v-if="title">
+            <h1 class="font-bold text-left text-black text-2xl capitalize">
+                {{ title }}
+            </h1>
+
+            <p v-if="description" class="text-gray-500 dark:text-gray-300">
+                {{ description }}
+            </p>
+        </div>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ resources/js/Components/Container.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 w-full flex flex-col gap-4"
+    >
+        <slot />
+    </div>
+</template>
