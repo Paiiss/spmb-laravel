0baa222140d9ee61a8203542d5122d14b6dada73
Paiss <pais.2k05@gmail.com>
2024-02-19 00:44:07 +0700

    feat: adds health attributes

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -32,6 +32,11 @@ class Form extends Model implements HasMedia
         return $this->belongsTo(Prodi::class, 'option_id', 'id');
     }
 
+    public function health()
+    {
+        return $this->belongsTo(Health::class);
+    }
+
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('ktp')->singleFile();
