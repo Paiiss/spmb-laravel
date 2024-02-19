85bafd1fa4fd1b7b69370ccd2721bdb41d0e03db
Paiss <pais.2k05@gmail.com>
2024-02-19 15:12:43 +0700

    update: Add a rejection reason field

--- database/migrations/2023_11_32_092016_create_forms_table.php
+++ database/migrations/2023_11_32_092016_create_forms_table.php
@@ -101,9 +101,10 @@ return new class extends Migration {
             $table->boolean('is_via_online')->default(true);
             $table->boolean('is_lock')->default(false);
             $table->boolean('is_submitted')->default(false);
-            $table->enum('status', ['waiting', 'submitted', 'pending', 'approved', 'rejected'])->default('waiting');
-            $table->enum('end_status', ['pending', 'submitted', 'approved', 'rejected'])->default('pending');
             $table->text('note')->nullable();
+            $table->enum('status', ['waiting', 'submitted', 'pending', 'approved', 'rejected'])->default('waiting');
+            $table->text('reason_rejected')->nullable();
+            $table->enum('end_status', ['pending', 'submitted', 'approved', 'rejected'])->default('pending');
             $table->timestamp('is_paid_registration')->nullable();
             $table->boolean('is_paid_tuition')->default(false);
 
