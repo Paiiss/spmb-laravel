a9560e7fac4669edc8dbd33e6a1d89cd6e5680f9
Paiss <pais.2k05@gmail.com>
2024-02-19 12:13:06 +0700

    feat: component display

---
+++ resources/js/Components/Display.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <label class="text-gray-500">{{ label }}</label>
+    <p class="font-semibold">
+        {{ value }}
+    </p>
+</template>
