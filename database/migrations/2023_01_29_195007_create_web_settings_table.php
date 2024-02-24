0fe01a0550179626cbf81055ae0f241b6391fa05
Paiss <pais.2k05@gmail.com>
2024-02-24 14:00:43 +0700

    update: websetting

--- database/migrations/2023_01_29_195007_create_web_settings_table.php
+++ database/migrations/2023_01_29_195007_create_web_settings_table.php
@@ -12,18 +12,27 @@ return new class extends Migration {
     {
         Schema::create('web_settings', function (Blueprint $table) {
             $table->id();
+
+            // Site
             $table->string('name')->nullable();
             $table->string('site_name')->nullable();
             $table->string('short_name')->nullable();
-            $table->string('institution_name')->nullable();
-            $table->string('institution_synopsis')->nullable();
             $table->string('title_home')->nullable();
             $table->string('title_dashboard')->nullable();
             $table->string('title_exam')->nullable();
 
+            // Institution
+            $table->string('institution_name')->nullable();
+            $table->string('institution_short_name')->nullable();
+            $table->string('institution_synopsis')->nullable();
+            $table->text('institution_vision_mission')->nullable();
+            $table->text('institution_history')->nullable();
+
+            // Footer
             $table->text('footer')->nullable();
             $table->string('link_univ')->nullable();
 
+            // Contact
             $table->string('contact_telp')->nullable();
             $table->string('contact_email')->nullable();
             $table->string('contact_fax')->nullable();
@@ -50,7 +59,10 @@ return new class extends Migration {
             'site_name' => 'PMB Hang Tuah',
             'short_name' => 'PMB HTP',
             'institution_name' => 'Universitas Hang Tuah Pekanbaru',
+            'institution_short_name' => 'UHTP',
             'institution_synopsis' => 'UHT Pekanbaru mempunyai tata nilai budaya akademik yang khas yaitu BERTUAH mempunyai makna bahwa setiap lulusan Universitas Hang Tuah Pekanbaru harus memiliki kualifikasi',
+            'institution_vision_mission' => 'Visi Universitas Hang Tuah Pekanbaru adalah mencapai keunggulan universitas yang menghasilkan lulusan kompeten bersaing di Asia Tenggara 2036. Misi meliputi pembelajaran, penelitian, pengabdian, dan kerjasama. Tujuannya menghasilkan lulusan berkualitas, karya ilmiah teruji, pengabdian yang berdampak, dan kerjasama produktif. Sasaran termasuk pendidikan, penelitian, manajemen, penjaminan mutu, dan kehidupan akademik. Motto: "Berkualitas, Teruji, Unggul".',
+            'institution_history' => 'Universitas Hang Tuah Pekanbaru (UHTP) adalah perguruan tinggi swasta di Pekanbaru, Riau, Indonesia. UHTP didirikan pada tahun 2003. UHTP memiliki 3 fakultas, yaitu Fakultas Kesehatan, Ilmu Komputer, dan Komunikasi & hukum',
             'title_home' => 'Hang Tuah Pekanbaru',
             'title_dashboard' => 'PMB',
             'title_exam' => 'CBT',
@@ -60,7 +72,7 @@ return new class extends Migration {
             'contact_fax' => '+ 0761 33815',
             'contact_address' => 'Jl. Mustafa Sari No.5, Tengkerang Sel., Kec. Bukit Raya, Kota Pekanbaru, Riau 28281',
             'contact_maps' => 'https://www.google.com/maps/place/Universitas+Hang+Tuah+Pekanbaru/@0.4826435,101.4582083,16z/data=!4m6!3m5!1s0x31d5aee753f76eb1:0x659cba40a618901b!8m2!3d0.4827927!4d101.4563443!16s%2Fg%2F1tgpvq07?entry=ttu',
-            'contact_maps_link' => 'https://www.google.com/maps/embed/v1/place?q=place_id:ChIJsW73U-eu1TERG5AYpkC6nGU&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8',
+            'contact_maps_link' => 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15958.706127869089!2d101.46361704838738!3d0.48303952682052753!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x659cba40a618901b!2sSTIKES+and+STMIK+Hang+Tuah!5e0!3m2!1sen!2sid!4v1495437561708',
             'contact_facebook' => 'https://www.facebook.com/UniversitasHangTuahPekanbaru',
             'contact_whatsapp' => '+ 0761 33815',
             'contact_instagram' => 'https://www.instagram.com/univ.hangtuahpekanbaru',
