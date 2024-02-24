ba68293924924fc3b52eaebe4c02b44f92b81ae4
Paiss <pais.2k05@gmail.com>
2024-02-25 01:59:20 +0700

    feat: adds scroll interaction

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const appName = import.meta.env.VITE_APP_NAME || "PMB";
 
@@ -19,28 +21,32 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(AOS)
             .mount(el)
             .$nextTick(() => {
-                $(".owl-carousel").owlCarousel({
-                    loop: true,
-                    margin: 10,
-                    items: 3,
-                    dots: true,
-                    autoplay: true,
-                    autoplayTimeout: 5000,
-                    autoplayHoverPause: true,
-                    stagePadding: 50,
-                    responsive: {
-                        0: {
-                            items: 1,
+                AOS.init(); // initialize AOS
+                $(document).ready(function () {
+                    $(".owl-carousel").owlCarousel({
+                        loop: true,
+                        margin: 10,
+                        items: 3,
+                        dots: true,
+                        autoplay: true,
+                        autoplayTimeout: 5000,
+                        autoplayHoverPause: true,
+                        stagePadding: 50,
+                        responsive: {
+                            0: {
+                                items: 1,
+                            },
+                            600: {
+                                items: 2,
+                            },
+                            1000: {
+                                items: 3,
+                            },
                         },
-                        600: {
-                            items: 2,
-                        },
-                        1000: {
-                            items: 3,
-                        },
-                    },
+                    });
                 });
             });
     },
