4599f4766be7d368a793b4f9ccdc095746eef1d6
Paiss <pais.2k05@gmail.com>
2024-02-25 18:09:07 +0700

    feat: create loading components

---
+++ resources/js/Components/Loading.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { AtomSpinner } from "epic-spinners";
+import { ref, onMounted } from "vue";
+
+const loading = ref(true);
+
+onMounted(() => {
+    setTimeout(() => {
+        loading.value = false;
+    }, 3000);
+});
+</script>
+
+<template>
+    <div
+        class="fixed top-0 z-50 h-screen w-screen flex justify-center items-center bg-white transition-opacity duration-1000 ease-in-out"
+        :class="{ 'opacity-0': !loading }"
+    >
+        <atom-spinner :animation-duration="1000" :size="60" color="#21B1A1" />
+    </div>
+</template>
