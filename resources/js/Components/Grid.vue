cc143851e803ed8cdc9e0b0421e1fcbc23b9a7ee
Paiss <pais.2k05@gmail.com>
2024-02-26 19:44:45 +0700

    feat: create grid components

---
+++ resources/js/Components/Grid.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    col: {
+        type: String,
+        default: "1",
+    },
+    sm: {
+        type: String,
+        default: null,
+    },
+    md: {
+        type: String,
+        default: null,
+    },
+    lg: {
+        type: String,
+        default: null,
+    },
+    xl: {
+        type: String,
+        default: null,
+    },
+    gap: {
+        type: String,
+        default: "4",
+    },
+});
+
+const className = computed(() => {
+    return [
+        "grid",
+        `grid-cols-${props.col}`,
+        props.sm ? `sm:grid-cols-${props.sm}` : "",
+        props.md ? `md:grid-cols-${props.md}` : "",
+        props.lg ? `lg:grid-cols-${props.lg}` : "",
+        props.xl ? `xl:grid-cols-${props.xl}` : "",
+        `gap-${props.gap}`,
+    ];
+});
+</script>
+
+<template>
+    <div :class="className" class="w-full">
+        <slot />
+    </div>
+</template>
