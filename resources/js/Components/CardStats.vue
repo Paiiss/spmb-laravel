d1cf9b4381a073d2d4b73c4d759f09c33f048302
Paiss <pais.2k05@gmail.com>
2024-03-01 10:10:30 +0700

    feat: stat card components

---
+++ resources/js/Components/CardStats.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+    label: {
+        type: String,
+        default: "Label",
+    },
+    icon: {
+        type: String,
+        default: "fa-solid fa-question",
+    },
+    color: {
+        type: String,
+        default: "gray",
+    },
+    value: {
+        type: String,
+        default: "0",
+    },
+});
+</script>
+
+<template>
+    <div
+        class="flex items-center justify-between p-4 bg-white rounded-lg shadow-md"
+    >
+        <div class="p-4 rounded-full mr-4" :class="`bg-${color}-100`">
+            <i class="text-4xl" :class="`${icon} text-${color}-500`" />
+        </div>
+        <div class="flex flex-col justify-center items-center">
+            <div
+                class="text-2xl font-bold flex w-full justify-end md:justify-center"
+            >
+                {{ value }}
+            </div>
+            <div>{{ label }}</div>
+        </div>
+    </div>
+</template>
